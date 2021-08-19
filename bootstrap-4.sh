ab9b18212949bf0b92ba3970d1716928025c6ba8
Erik Reinertsen <erikrtn@gmail.com>
2021-08-19 12:29:49 -0400

    Revise init.vim

---
+++ bootstrap-4.sh
@@ -0,0 +1,17 @@
+nvim --headless +PlugInstall +qall
+
+# Conda & Mamba
+curl -L -O https://github.com/conda-forgeminiforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh
+bash Mambaforge-$(uname)-$(uname -m).sh
+
+# Base DSE environment
+mamba env create -f ~/bootstrap/environment.yml
+
+# Speedtest
+curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
+sudo apt-get install speedtest
+
+# AWS CLI 2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install/
