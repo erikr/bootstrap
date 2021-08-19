a118afba44ad9199db75810452f30df2b27d0d84
Erik Reinertsen <erikrtn@gmail.com>
2021-08-19 12:25:20 -0400

    revise bootstrap

--- bootstrap-1.sh
+++ bootstrap-1.sh
@@ -8,4 +8,6 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install neovim curl zsh tmux tree unzip make
 sudo apt-get dist-upgrade
-sudo passwd
+
+# OMZ
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
