d21920efd34458b070048086654a0f8614a4776a
Erik Reinertsen <erikrtn@gmail.com>
2021-08-20 16:28:57 -0400

    Linux postinstall for docker

--- bootstrap-docker.sh
+++ bootstrap-docker.sh
@@ -20,3 +20,7 @@ sudo docker run hello-world
 # Docker compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+# Linux post-install to run docker commands without sudo
+sudo groupadd docker
+sudo usermod -aG docker $USER
